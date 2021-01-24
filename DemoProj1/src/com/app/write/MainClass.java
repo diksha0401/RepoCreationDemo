09c29422ffd6c9dd3cfb671316887af087fb2da4
diksha <dikshashiinde011@gmail.com>
2021-01-23 20:30:36 -0800

    Team Lead added wish method

--- DemoProj1/src/com/app/write/MainClass.java
+++ DemoProj1/src/com/app/write/MainClass.java
@@ -3,5 +3,9 @@ package com.app.write;
 public class MainClass {
 public static void main(String[] args) {
 	System.out.println("Hello World.....abcdtl");
+	wish();
+}
+public static void wish() {
+	System.out.println("Good Morning");
 }
 }
