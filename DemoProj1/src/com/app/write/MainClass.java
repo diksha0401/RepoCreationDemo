1567989590fa467b3f71907e3094cda7417f7f79
diksha <dikshashiinde011@gmail.com>
2021-01-24 09:50:32 -0800

    after ignore

--- DemoProj1/src/com/app/write/MainClass.java
+++ DemoProj1/src/com/app/write/MainClass.java
@@ -3,5 +3,6 @@ package com.app.write;
 public class MainClass {
 public static void main(String[] args) {
 	System.out.println("Hello WorldBy Developer.......bc123dev");
+	System.out.println("After ignore");
 }
 }
